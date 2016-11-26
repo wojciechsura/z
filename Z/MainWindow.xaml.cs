4c8f77c823281c97152085aff78099f5e82944e0
Wojciech Sura <wojciechsura@gmail.com>
2016-11-26 12:45:01 +0100

    First commit

---
+++ Z/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Z.Common;
+using Z.Viewmodels;
+
+namespace Z
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        // Private fields -----------------------------------------------------
+
+        private readonly MainWindowViewModel viewModel;
+
+        // Public methods -----------------------------------------------------
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.viewModel = new MainWindowViewModel();
+            this.DataContext = viewModel;
+        }
+    }
+}
---
+++ Z/Viewmodels/MainWindowViewModel.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Z.Viewmodels
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        private string enteredText;
+        private bool keywordVisible;
+
+        // Protected methods --------------------------------------------------
+
+        protected void OnPropertyChanged(string name)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(name));
+            }
+        }
+
+        // Public methods -----------------------------------------------------
+
+        public MainWindowViewModel()
+        {
+            keywordVisible = false;
+            enteredText = null;
+        }
+
+        // Public properties --------------------------------------------------
+
+        public string EnteredText
+        {
+            get
+            {
+                return enteredText;
+            }
+            set
+            {
+                enteredText = value;
+                OnPropertyChanged(nameof(EnteredText));
+            }
+        }
+
+        public bool KeywordVisible
+        {
+            get
+            {
+                return keywordVisible;
+            }
+            set
+            {
+                keywordVisible = value;
+                OnPropertyChanged(nameof(KeywordVisible));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        internal void HotkeyPressed()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
