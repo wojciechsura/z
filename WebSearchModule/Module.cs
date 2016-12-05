54822110f5a3e857bc97cf665860c61b186cef9a
wsura <wsura@pgs-soft.com>
2016-12-05 09:27:04 +0100

    Added new search engines

--- WebSearchModule/Module.cs
+++ WebSearchModule/Module.cs
@@ -12,12 +12,31 @@ namespace WebSearchModule
 {
     public class Module : IZModule
     {
+        private class SearchInfo       
+        {
+            public SearchInfo(string action, string keyword, string display, string searchString)
+            {
+                Action = action;
+                Keyword = keyword;
+                Display = display;
+                SearchString = searchString;
+            }
+
+            public string Action { get; private set; }
+            public string Keyword { get; private set; }
+            public string Display { get; private set; }
+            public string SearchString { get; private set; }
+        }
+
         private const string MODULE_NAME = "WebSearch";
 
-        private const string SEARCH_ACTION = "GoogleSearch";
-        private const string SEARCH_KEYWORD = "g";
-        private const string SEARCH_DISPLAY = "Google";
-        private const string GoogleSearchString = "https://www.google.com/#q={0}";
+        private List<SearchInfo> searchInfos = new List<SearchInfo>
+        {
+            new SearchInfo("GoogleSearch", "g", "Google", "https://www.google.com/#q={0}"),
+            new SearchInfo("WikipediaSearch", "wiki", "Wikipedia", "http://pl.wikipedia.org/w/index.php?title=Specjalna:Szukaj&search={0}"),
+            new SearchInfo("MSDNSearch", "msdn", "MSDN", "http://social.msdn.microsoft.com/Search/en-EN?query={0}"),
+            new SearchInfo("YoutubeSearch", "y", "Youtube", "http://www.youtube.com/results?search_query={0}&page={{startPage?}}&utm_source=opensearch")
+        };
 
         public string InternalName
         {
@@ -29,24 +48,22 @@ namespace WebSearchModule
 
         public IEnumerable<KeywordActionInfo> GetKeywordActions()
         {
-            return new List<KeywordActionInfo>
-            {
-                new KeywordActionInfo(SEARCH_KEYWORD, SEARCH_ACTION, SEARCH_DISPLAY)                
-            };
+            return searchInfos
+                .Select(si => new KeywordActionInfo(si.Keyword, si.Action, si.Display))
+                .ToList();
         }
 
         public void ExecuteKeywordAction(string action, string expression)
         {
-            switch (action)
+            var info = searchInfos
+                .FirstOrDefault(i => i.Action == action);
+
+            if (info != null)
             {
-                case SEARCH_ACTION:
-                    {
-                        Process.Start(String.Format(GoogleSearchString, WebUtility.UrlEncode(expression)));
-                        break;
-                    }
-                default:
-                    throw new InvalidOperationException("Invalid action!");
+                Process.Start(String.Format(info.SearchString, WebUtility.UrlEncode(expression)));
             }
+            else
+                    throw new InvalidOperationException("Invalid action!");
         }
     }
 }
