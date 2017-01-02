5f684c62cff9b16b3e1f3a1c31e0ed8e2a3f11bf
Wojciech Sura <wojciechsura@gmail.com>
2017-01-02 11:26:50 +0100

    Shell folders module

---
+++ ShellFoldersModule/Infrastructure/WinApiInterop.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShellFoldersModule.Infrastructure
+{
+    static class WinApiInterop
+    {
+        [DllImport("shell32.dll", CallingConvention = CallingConvention.Winapi, CharSet = CharSet.Unicode)]
+        internal static extern int SHGetLocalizedName(string pszPath, StringBuilder pszResModule, ref int cch, out int pidsRes);
+
+        [DllImport("user32.dll", EntryPoint = "LoadStringW", CallingConvention = CallingConvention.Winapi, CharSet = CharSet.Unicode)]
+        internal static extern int LoadString(IntPtr hModule, int resourceID, StringBuilder resourceValue, int len);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Unicode, ExactSpelling = true, EntryPoint = "LoadLibraryExW")]
+        internal static extern IntPtr LoadLibraryEx(string lpFileName, IntPtr hFile, uint dwFlags);
+
+        internal const uint DONT_RESOLVE_DLL_REFERENCES = 0x00000001;
+        internal const uint LOAD_LIBRARY_AS_DATAFILE = 0x00000002;
+
+        [DllImport("kernel32.dll", ExactSpelling = true)]
+        internal static extern int FreeLibrary(IntPtr hModule);
+
+        [DllImport("kernel32.dll", EntryPoint = "ExpandEnvironmentStringsW", CharSet = CharSet.Unicode, ExactSpelling = true)]
+        internal static extern uint ExpandEnvironmentStrings(string lpSrc, StringBuilder lpDst, int nSize);
+
+        public static string GetFullPath(string path)
+        {
+            StringBuilder sb = new StringBuilder(1024);
+
+            ExpandEnvironmentStrings(path, sb, sb.Capacity);
+
+            return sb.ToString();
+        }
+
+        public static string GetLocalizedName(string path)
+        {
+            StringBuilder resourcePath = new StringBuilder(1024);
+            StringBuilder localizedName = new StringBuilder(1024);
+            int len, id;
+            len = resourcePath.Capacity;
+
+            if (SHGetLocalizedName(path, resourcePath, ref len, out id) == 0)
+            {               
+                ExpandEnvironmentStrings(resourcePath.ToString(), resourcePath, resourcePath.Capacity);
+                IntPtr hMod = LoadLibraryEx(resourcePath.ToString(), IntPtr.Zero, DONT_RESOLVE_DLL_REFERENCES | LOAD_LIBRARY_AS_DATAFILE);
+                if (hMod != IntPtr.Zero)
+                {
+                    if (LoadString(hMod, id, localizedName, localizedName.Capacity) != 0)
+                    {
+                        return localizedName.ToString();
+                    }
+                    FreeLibrary(hMod);
+                }
+            }
+
+            return null;
+        }
+    }
+}
---
+++ ShellFoldersModule/Module.cs
@@ -0,0 +1,192 @@
+﻿using Microsoft.Win32;
+using ShellFoldersModule.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Z.Api;
+using System.Windows.Media;
+using Z.Api.Interfaces;
+using Z.Api.Types;
+using System.Diagnostics;
+using Microsoft.WindowsAPICodePack.Shell;
+using System.Windows.Media.Imaging;
+
+namespace ShellFoldersModule
+{
+    public class Module : IZModule
+    {
+        private class ShellFolderInfo
+        {
+            public ShellFolderInfo(string key, string display, string comment, string command)
+            {
+                Key = key;
+                Display = display;
+                Comment = comment;
+                CanonicalName = command;
+            }
+
+            public string Key { get; private set; }
+            public string Display { get; private set; }
+            public string Comment { get; private set; }
+            public string CanonicalName { get; private set; }
+        }
+
+        private const string MODULE_NAME = "ShellFolders";
+        private const string MODULE_DISPLAY_NAME = "Shell folder";
+
+        private const string SHELL_ACTION_KEYWORD = "shell";
+        private const string SHELL_ACTION_NAME = "ShellFolder";
+        private const string SHELL_ACTION_DISPLAY = "Shell folder";
+        private const string SHELL_ACTION_COMMENT = "Open shell folder";
+
+        private List<ShellFolderInfo> infos;
+        private ImageSource icon;
+
+        private void LoadShellFoldersFromRegistry()
+        {
+            // Shell folders from registry
+
+            var shellFolders = Registry.CurrentUser.OpenSubKey(@"Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders");
+            foreach (string name in shellFolders.GetValueNames())
+            {
+                var folder = shellFolders.GetValue(name) as string;
+
+                var fullPath = WinApiInterop.GetFullPath(folder);
+                var key = Path.GetFileName(fullPath);
+                var displayName = WinApiInterop.GetLocalizedName(fullPath);
+
+                infos.Add(new ShellFolderInfo(key, displayName, fullPath, fullPath));
+            }
+        }
+
+        private static string GenerateLocalizedName(IKnownFolder shellFolder)
+        {
+            string localizedName = shellFolder.LocalizedName;
+
+            if (String.IsNullOrEmpty(localizedName) && Directory.Exists(shellFolder.Path))
+            {
+                try
+                {
+                    localizedName = WinApiInterop.GetLocalizedName(shellFolder.Path);
+                }
+                catch
+                {
+
+                }
+            }
+
+            if (String.IsNullOrEmpty(localizedName) && Directory.Exists(shellFolder.Path))
+                localizedName = Path.GetFileName(shellFolder.Path);
+
+            if (String.IsNullOrEmpty(localizedName))
+                localizedName = shellFolder.CanonicalName;
+            return localizedName;
+        }
+
+        private void LoadShellFolders()
+        {
+            foreach (var shellFolder in KnownFolders.All)
+            {
+                string localizedName = GenerateLocalizedName(shellFolder);
+
+                string comment = shellFolder.PathExists ? shellFolder.Path : $"shell:{shellFolder.CanonicalName}";
+
+                infos.Add(new ShellFolderInfo(shellFolder.CanonicalName,
+                    localizedName,
+                    comment,
+                    shellFolder.CanonicalName));
+            }
+        }
+
+        public void CollectSuggestions(string enteredText, string action, bool perfectMatchesOnly, ISuggestionCollector collector)
+        {
+            Func<ShellFolderInfo, bool> func;
+
+            if (perfectMatchesOnly)
+                func = (sfi => sfi.Key.ToUpper() == enteredText.ToUpper() || sfi.Display.ToUpper() == enteredText.ToUpper());
+            else
+                func = sfi => sfi.Key.ToUpper().Contains(enteredText.ToUpper()) || sfi.Display.ToUpper().Contains(enteredText.ToUpper());
+
+            infos.Where(func)
+                .OrderBy(sfi => sfi.Display)
+                .ToList()
+                .ForEach(sfi => collector.AddSuggestion(new SuggestionInfo(sfi.Key, sfi.Display, sfi.Comment, icon, sfi.CanonicalName)));
+        }
+
+        public void ExecuteKeywordAction(string action, string expression, ExecuteOptions options)
+        {
+            var info = infos.Where(i => i.Key.ToUpper() == expression.ToUpper() || i.Display.ToUpper() == expression.ToUpper());
+            if (info.Count() == 1)
+            {
+                try
+                {
+                    Process.Start($"shell:{info.First().CanonicalName}");
+                }
+                catch
+                {
+                    // TODO notify about error
+                    options.PreventClose = true;
+                }
+            }
+            else
+            {
+                // TODO notify about error
+                options.PreventClose = true;
+            }
+        }
+
+        public void ExecuteSuggestion(SuggestionInfo suggestion, ExecuteOptions options)
+        {
+            try
+            {
+                string canonicalName = suggestion.Data as string;
+                Process.Start($"shell:{canonicalName}");
+            }
+            catch
+            {
+                // TODO notify about error
+                options.PreventClose = true;
+            }
+        }
+
+        public IEnumerable<KeywordInfo> GetKeywordActions()
+        {
+            yield return new KeywordInfo(SHELL_ACTION_KEYWORD, SHELL_ACTION_NAME, SHELL_ACTION_DISPLAY, SHELL_ACTION_COMMENT);
+        }
+
+        public Module()
+        {
+            infos = new List<ShellFolderInfo>();
+            LoadShellFolders();
+
+            icon = new BitmapImage(new Uri("pack://application:,,,/ShellFoldersModule;component/Resources/shell.png"));
+        }
+
+        public string Name
+        {
+            get
+            {
+                return MODULE_NAME;
+            }
+        }
+
+        public string DisplayName
+        {
+            get
+            {
+                return MODULE_DISPLAY_NAME;
+            }
+        }
+
+        public ImageSource Icon
+        {
+            get
+            {
+                return icon;
+            }
+        }
+    }
+}
