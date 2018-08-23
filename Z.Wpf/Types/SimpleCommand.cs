47843a70f5bcb52d1f4c48f5e04de09bdef33816
wsura <wsura@WSURA-WRO>
2018-08-23 10:54:48 +0200

    Refactoring for multiple windows in progress

---
+++ Z.Wpf/Types/SimpleCommand.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Z.Wpf.Types
+{
+    public class SimpleCommand : ICommand
+    {
+        private readonly Action<object> action;
+        private readonly Func<object, bool> canExecute;
+
+        protected virtual void OnCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+
+        public SimpleCommand(Action<object> action)
+        {
+            if (action == null)
+                throw new ArgumentNullException(nameof(action));
+
+            this.action = action;
+            this.canExecute = (o) => true;
+        }
+
+        public SimpleCommand(Action<object> action, Func<object, bool> canExecute)
+        {
+            if (action == null)
+                throw new ArgumentNullException(nameof(action));
+            if (canExecute == null)
+                throw new ArgumentNullException(nameof(canExecute));
+
+            this.action = action;
+            this.canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return canExecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            action(parameter);
+        }
+
+        public void NotifyCanExecuteChanged()
+        {
+            OnCanExecuteChanged();
+        }
+
+        public event EventHandler CanExecuteChanged;
+    }
+}
