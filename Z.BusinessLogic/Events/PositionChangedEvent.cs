48321eaadcaef283be10e9a264db72b5ae25c6e3
Wojciech Sura <wojciechsura@gmail.com>
2018-08-27 20:45:45 +0200

    Added position sync

---
+++ Z.BusinessLogic/Events/PositionChangedEvent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Z.BusinessLogic.Events
+{
+    public class PositionChangedEvent : BaseEvent
+    {
+        public PositionChangedEvent(int x, int y, object origin)
+        {
+            X = x;
+            Y = y;
+            Origin = origin;
+        }
+
+        public object Origin { get; private set; }
+        public int X { get; private set; }
+        public int Y { get; private set; }
+    }
+}
