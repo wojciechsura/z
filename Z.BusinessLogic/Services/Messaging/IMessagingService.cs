00f8826068e2168a4c785692a38029b8ce62735f
Wojciech Sura <wojciechsura@gmail.com>
2020-03-12 17:17:49 +0100

    Added icons to launcher shortcuts

---
+++ Z.BusinessLogic/Services/Messaging/IMessagingService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Z.BusinessLogic.Services.Messaging
+{
+    public interface IMessagingService
+    {
+        bool AskYesNo(string message, string title = null);
+        bool? AskYesNoCancel(string message, string title = null);
+        void Warn(string message, string title = null);
+        bool WarnYesNo(string message, string title = null);
+        void ShowError(string message, string title = null);
+        void Inform(string message, string title = null);
+        void Beep();
+    }
+}
